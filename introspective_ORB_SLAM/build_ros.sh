d728173fb0189f8a5866c55f8987cc8baaa16bf6
JackBorer <jackborer@utexas.edu>
2021-02-01 11:56:44 -0600

    Torch dependency

--- introspective_ORB_SLAM/build_ros.sh
+++ introspective_ORB_SLAM/build_ros.sh
@@ -4,5 +4,5 @@ cd Examples/ROS/ORB_SLAM2
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:`pwd`
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
+cmake .. -DROS_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=../../../Thirdparty/libtorch
 make -j
